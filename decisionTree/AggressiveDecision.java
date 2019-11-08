2764d62c1ca5051e6c96af5add677977906dc2a6
Jaemin Kim <geminik23@gmail.com>
2019-11-08 02:10:26 +0000

    fixed

--- decisionTree/AggressiveDecision.java
+++ decisionTree/AggressiveDecision.java
@@ -9,7 +9,7 @@ public class AggressiveDecision extends BaseDecision {
 
    @Override
    protected DecisionTreeNode getBranch(DecisionParams param) {
-       if (param.ammo == 0 && param.min_enemy <= DecisionParams.DISTANT_MEASUREMENT
+       if (param.ammo != 0 && param.min_enemy <= DecisionParams.DISTANT_MEASUREMENT
        && param.min_bomb > DecisionParams.DISTANT_MEASUREMENT){
            return trueNode;
        }
